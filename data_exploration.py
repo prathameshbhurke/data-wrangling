245b7dead1a37920a0fe9ce8e696e8b9850db49f
Prathamesh Bhurke <pbhurke1990@gmail.com>
2023-03-15 15:53:29 -0700

    1st function created

---
+++ data_exploration.py
@@ -0,0 +1,17 @@
+
+from sql_etl import create_table
+import pandas as pd
+import numpy as np
+
+class data_ingestion():
+    def createtable():
+        col_list = ['firstname VARCHAR(255)', 'lastname VARCHAR(255)']  # and so on
+        create_table('my_table2',col_list);
+
+
+if __name__ == '__main__':
+    data_ingestion.createtable()
+
+
+
+
