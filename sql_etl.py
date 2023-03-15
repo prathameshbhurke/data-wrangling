d1f3d0b3c182a917e5ef0659a2d641d11249c62d
Prathamesh Bhurke <pbhurke1990@gmail.com>
2023-03-15 15:51:30 -0700

    minor updates

---
+++ sql_etl.py
@@ -0,0 +1,73 @@
+import psycopg2
+from config import config
+
+
+def connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # read connection parameters
+        params = config()
+
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+
+        # create a cursor
+        cur = conn.cursor()
+
+        # execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+
+        # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+
+def create_table():
+    conn = None
+    try:
+
+        # create statement
+        #create_sql = 'CREATE TABLE ' + table_name + '(' + ','.join(my_reader.fieldnames) + ')'
+        # read database configuration
+        params = config()
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        cur.execute("""
+        CREATE TABLE customer2(
+        customer_id INT PRIMARY KEY NOT NULL,
+        name CHAR(50) NOT NULL,
+        address CHAR(100),
+        email CHAR(50),
+        phone_number CHAR(20));
+        """)
+        print('Table created in PostgreSQL')
+
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+if __name__ == '__main__':
+    connect()
+    create_table()
